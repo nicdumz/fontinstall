2b6b7cd1a39b53e6ac0e2a76ee9661b3fb6fd022
Nicolas Dumazet <nicdumz.commits@gmail.com>
2015-08-08 19:39:11 +0200

    script fixes

--- fontinstall.sh
+++ fontinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Usage:
 #  ./fontinstall foo/*.ttf
-if [ -d "~/Library" ]; then
+if [ -d $HOME/Library ]; then
     # Poor man's OS detection -- this is Mac.
-    mkdir ~/Library/Fonts
-    cp $@ ~/Library/Fonts
+    mkdir -p ~/Library/Fonts
+    cp "$@" ~/Library/Fonts
 else
-    mkdir ~/.fonts
-    cp $@ ~/.fonts
+    mkdir -p ~/.fonts
+    cp "$@" ~/.fonts
     fc-cache -vf ~/.fonts
 fi
